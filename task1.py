2a88805a57a886937bdd85719a5bb41f767ff1ef
Fahad <fhdnaufal@gmail.com>
2024-07-01 23:55:44 +0530

    first commit

---
+++ task1.py
@@ -0,0 +1,23 @@
+list1=[1,2,3,4,5,6,7]
+
+list1.append(9)
+
+list1.pop()
+
+list1.remove(5)
+
+newDict={'name':'fahad','task':1}
+
+newDict['domain']='dataScience'
+
+newDict['task']=2
+
+del newDict['name']
+
+newSet={1,2,3,4,5,6,7}
+
+newSet.add(8)
+
+newSet.remove(5)
+
+print(newSet)
